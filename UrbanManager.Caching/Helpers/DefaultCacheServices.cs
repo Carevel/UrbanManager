6254d5ed4d115746149a3266b7a8ba602c2b6940
Carevel <872287340@qq.com>
2016-04-10 15:57:39 +0800

    add ICacheService

---
+++ UrbanManager.Caching/Helpers/DefaultCacheServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrbanManager.Caching.Services;
+
+namespace UrbanManager.Caching.Helpers
+{
+    public class DefaultCacheServices : ICacheService
+    {
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetObject<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put<T>(string key, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put<T>(string key, T value, TimeSpan validFor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UrbanManager.Caching/Services/ICacheService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanManager.Caching.Services
+{
+    public interface ICacheService
+    {
+        object GetObject<T>(string key);
+        void Put<T>(string key, T value);
+        void Put<T>(string key, T value, TimeSpan validFor);
+        void Remove(string key);
+        void Clear();
+    }
+}
