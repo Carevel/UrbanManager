7373f410016ca0a48745dbf80536e6e9c93a6412
Carevel <872287340@qq.com>
2016-04-10 16:15:41 +0800

    add ICommonFileSystem

---
+++ UrbanManager.FileSystem/ICommonFileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanManager.FileSystem
+{
+    public class ICommonFileSystem
+    {
+        string rootPath { get; set; }
+
+    }
+}
